e0d13df9d050b333a6f8bbabba696df955859594
Ryan Blakeley <rbblakeley@gmail.com>
2018-06-18 17:15:12 -0500

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-6deb425685327bab97d9.js","5aba05f58ca1dd7f84fa38dfded7e89f"],["/commons-b3fa58a2f2cbb2928d6e.js","8b0a91b28ed153631b159dd61bcbd3f9"],["/index.html","64e7975957e73c4198c2c79a699e46bb"],["/offline-plugin-app-shell-fallback/index.html","3740ecc6a053fe9ee620f687b65030cf"]];
+var precacheConfig = [["/app-6deb425685327bab97d9.js","5aba05f58ca1dd7f84fa38dfded7e89f"],["/commons-b3fa58a2f2cbb2928d6e.js","8b0a91b28ed153631b159dd61bcbd3f9"],["/index.html","e494e00bda85cfe40cc9ccc9d410f24d"],["/offline-plugin-app-shell-fallback/index.html","713b05bd1c33b9306a5ec5a404cfeb76"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
